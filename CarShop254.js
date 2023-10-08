d0c597852fc66ac2560a5406f5d1a40e6e5a42fc
Gad-Ongoro <gadongoro1@gmail.com>
2023-10-08 10:30:45 +0300

    Edited

--- CarShop254.js
+++ CarShop254.js
@@ -1,5 +1,12 @@
 let form = document.querySelector("form");
+let body = document.querySelector("body");
+let light = document.querySelector("img.light");
+let dark = document.querySelector("img.dark");
+let userInfo = document.querySelector("div.user-info");
 
+console.log(userInfo);
+
+//console.log(dark);
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -9,3 +16,15 @@ form.addEventListener("submit", (e) => {
         alert("Order cancelled");
     };
 });
+
+function lightDarkMode () {
+    light.addEventListener("click", function(e) {
+        body.style.backgroundColor = "rgba(255, 255, 255)";
+        userInfo.style.color = "navy";
+    })
+    dark.addEventListener("click", function(e) {
+        body.style.backgroundColor = "rgba(0, 20, 100)";
+        userInfo.style.color = "white";
+    })
+};
+lightDarkMode();
