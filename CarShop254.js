e8b583b06496540f99b6c4477a100c05f13133d0
Gad-Ongoro <gadongoro1@gmail.com>
2023-10-09 11:24:17 +0300

    Edited

--- CarShop254.js
+++ CarShop254.js
@@ -4,6 +4,7 @@ let light = document.querySelector("img.light");
 let dark = document.querySelector("img.dark");
 let userInfo = document.querySelector("div.user-info");
 let userInfoInput = document.querySelectorAll("div#user-info input");
+let carInfoP = document.querySelectorAll("div.eachCar p");
 
 console.log(userInfoInput);
 
@@ -22,9 +23,14 @@ function lightDarkMode () {
     light.addEventListener("click", function(e) {
         body.style.backgroundColor = "rgba(255, 255, 255)";
         userInfo.style.color = "navy";
+
         userInfoInput.forEach(function(userInput){
             userInput.style.border = "2px solid rgb(222, 111, 250, 0.6)";
         });
+
+        carInfoP.forEach(function(p){
+            p.style.color = "navy";
+        })
     })
     dark.addEventListener("click", function(e) {
         body.style.backgroundColor = "rgba(0, 20, 100)";
@@ -33,6 +39,10 @@ function lightDarkMode () {
         userInfoInput.forEach(function(userInput){
             userInput.style.border = "none";
         });
+
+        carInfoP.forEach(function(p){
+            p.style.color = "white";
+        })
     })
 };
 lightDarkMode();
